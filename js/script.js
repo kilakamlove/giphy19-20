b0b105de8013e712d9b9e7fc371cf3c6d12e028f
kilakamlove <kameronlovelady1@gmail.com>
2020-02-11 23:11:41 +0000

    html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 
 console.log('hi');
 
-$('h1').click(function(){
+$('button').click(function(){
+    fetch("<https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY>")
+.then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log
+      });
+
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
