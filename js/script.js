1a07e038c82d1c120595445d52c4b067e1e1b03e
kilakamlove <kameronlovelady1@gmail.com>
2020-02-25 23:22:10 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@
 console.log('hi');
 
 $('button').click(function(){
-    fetch("<https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY>")
+    var input = $("input").val()
+    fetch("https://api.giphy.com/v1/gifs/search?q=" + input +"&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
 .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        console.log
+        console.log(data)
+        console.log(data.data[0].images.original.url)
+        $('body').append('<img src=' + data.data[0].images.original.url + '>');
+    
       });
+   
 
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
